30c79a8b81d8fff075e6e3451455a4b4cadb02b8
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-02-18 15:32:23 -0800

    edited shop.js from router.use to router.get since 404 page wasn't showing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,11 +11,12 @@ app.use(bodyParser.urlencoded());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 // using a catch all method
 app.use((req, res, next) => {
-  res.status(404).send("<h1>page not found </h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+  //   res.status(404).send("<h1>page not found </h1>");
 });
 
 app.listen(3000, () => console.log(`listening on localhost:3000`));
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,14 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 // use() allows to add middleware functions
 // takes in requestHandlers
-router.use("/", (req, res, next) => {
-  res.send("<h1> Hello from Express!</h1>");
+router.get("/", (req, res, next) => {
+  // path provides an absolute path depending on the OS
+  // path.join also joins for us so we don't need the /
+  // HOWEVER, __dirname provides an absolute path to routes so to go UP one, we need "../"
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
