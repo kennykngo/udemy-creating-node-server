35a9e77aa23240709342e7d2aef06fce979e2d01
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-02-18 16:39:31 -0800

    styled views/ html files

--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,15 @@ const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 // use() allows to add middleware functions
 // takes in requestHandlers
 router.get("/", (req, res, next) => {
   // path provides an absolute path depending on the OS
   // path.join also joins for us so we don't need the /
   // HOWEVER, __dirname provides an absolute path to routes so to go UP one, we need "../"
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
