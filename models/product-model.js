a7f344fb7d81c9005b8d602f2083296f86ba73e4
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-02-19 20:27:07 -0800

    created admin && shop directory

--- models/product-model.js
+++ models/product-model.js
@@ -1,4 +1,21 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const pathJoin = path.join(
+  path.dirname(require.main.filename),
+  "data", // directory to store
+  "products.json" // name that you want to store the file/create
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(pathJoin, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +23,17 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(pathJoin, JSON.stringify(products), (err) =>
+        console.log(err)
+      );
+    });
+
+    // MUST use arrow function or else the 'this' will point to the wrong object
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
