e50b10dd438edfa5d33bbbf4c74cc43911178866
Kenny Ngo <kenny.ngo.design@gmail.com>
2021-02-21 15:00:11 -0800

    edited product-detail, list, and their respected models and controllers

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,7 @@ router.get("/products", shopController.getProducts);
 // ex. /products/12514
 router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart);
 router.get("/orders", shopController.getOrders);
 router.get("/checkout", shopController.getCheckout);
 
